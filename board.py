596015bf5f8438cbfada1bcb9829e5f1c367d73e
JackM400 <jack.millar400@gmail.com>
2019-08-06 21:10:22 +0100

    board + display board

--- board.py
+++ board.py
@@ -8,3 +8,17 @@
 #  change player
 
 
+GameBoard = [
+    [" _ ", " _ ", " _ "],
+    [" _ ", " _ ", " _ "],
+    [" _ ", " _ ", " _ "]
+]
+
+
+def DisplayBoard():
+    print(GameBoard)
+    for i in GameBoard:
+        print(i)
+
+
+DisplayBoard()
