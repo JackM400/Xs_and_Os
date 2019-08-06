5e5144deeff9505f3df42a8c88d6a1bfaf975d5d
JackM400 <jack.millar400@gmail.com>
2019-08-06 21:35:51 +0100

    Assign player

--- board.py
+++ board.py
@@ -13,12 +13,37 @@ GameBoard = [
     [" _ ", " _ ", " _ "],
     [" _ ", " _ ", " _ "]
 ]
+player1: str = ""
+player2 = ""
+
+
+def playerName():
+    print("Player 1 input name:")
+    global player1
+    player1 = input()
+    print("Player 2 input name:")
+    global player2
+    player2 = input()
+    return player1, player2
 
 
 def DisplayBoard():
-    print(GameBoard)
+    # print(GameBoard)
     for i in GameBoard:
         print(i)
+    print('Player 1: ' + player1 + " is X")
+    print('Player 2: ' + player2 + " is O")
 
 
-DisplayBoard()
+def turnHandler():
+    print("Input position.\n")
+    position = input()
+
+
+def playGame():
+    playerName()
+    DisplayBoard()
+    turnHandler()
+
+
+playGame()
